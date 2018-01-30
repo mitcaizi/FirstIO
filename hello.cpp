d1750794308fba08a771c923e75bb25c4ac21471
mitcaizi <jling1@mail.csuchico.edu>
2018-01-29 17:43:53 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
+// This is a gingle-ling comment because it follows the two forward-slashes. Jingwen Ling
+
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout <<" Hi, my name is Jingwen" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
